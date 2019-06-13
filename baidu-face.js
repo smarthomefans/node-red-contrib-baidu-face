2952ccf90e8856b581108d372090efc5a7668277
孙善明 <sunshanming@zhuojianchina.com>
2019-06-13 16:18:39 +0800

    修复编写错误

--- baidu-face.js
+++ baidu-face.js
@@ -15,6 +15,9 @@ module.exports = RED => {
           // 合并值,未细想
           for (const key in config) { if (config[key] != '' && config[key] != null) { data[key] = config[key] } }
           
+          data.max_face_num = data.max_face_num || 5
+          data.max_score = data.max_score || 70
+          data.user_list = data.user_list || {}
           // 人脸识别
           const userInfo = await bd.searchFace(data)
           data.payload = userInfo
