e106a1d5cb66b02064bbf91ea8030cf05917d27b
孙善明 <sunshanming@zhuojianchina.com>
2019-08-12 16:52:38 +0800

    新增人体识别

--- baidu-face.js
+++ baidu-face.js
@@ -89,4 +89,33 @@ module.exports = RED => {
       })
     }
   })
+
+  //人体检测和属性识别
+  RED.nodes.registerType('baidu-body-attr', class {
+    constructor (config) {
+      const node = this
+      RED.nodes.createNode(node, config)
+      const baiduface = RED.nodes.getNode(config.baiduface)
+      node.on('input', async data => {
+        try {
+          const bd = new Baidu(node, baiduface)
+
+          // 合并值,未细想
+          for (const key in config) { if (config[key] != '' && config[key] != null) { data[key] = config[key] } }
+         
+          // 人体检测和属性识别
+          const personInfo = await bd.bodyAttr(data)
+          data.payload = personInfo
+          node.status({ text: `识别成功:${data._msgid}` })
+          node.send([data, null])
+        } catch (err) {
+          node.status({ text: err.message, fill: 'red', shape: 'ring' })
+          node.warn(err)
+          data.payload = {}
+          data.error_msg = err.message
+          node.send([null ,data]) 
+        }
+      })
+    }
+  })
 }
